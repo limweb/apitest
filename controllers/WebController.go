36d52aee1eb9acc50bef73b3431c6519220e5431
lim web <limweb@hotmail.com>
2021-03-07 22:22:38 +0700

    update apidoc

--- controllers/WebController.go
+++ controllers/WebController.go
@@ -12,7 +12,7 @@ import (
 // @id Message
 // plain json xml x-www-form-urlencoded
 // @produce plain
-// @response 200 {object} models.Transaction "OK"
+// @response 200 {object} utils.ResponseData "OK"
 // get post put delete patch
 // @router / [get]
 func Message(c *gin.Context) {
@@ -26,7 +26,7 @@ func Message(c *gin.Context) {
 // @description Ping for the service
 // @id Ping
 // @produce plain
-// @response 200 {object} models.Product "OK"
+// @response 200 {object} utils.ResponseData  "OK"
 // @router /ping [get]
 func Ping(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
